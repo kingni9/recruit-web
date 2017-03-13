15e5d53ed4a0fc879200eefa60485a8d2ee47aad
zhuangjiatao <zhuangjiatao@qipeipu.com>
2017-03-13 18:34:27 +0800

    update

--- recruit-web-biz/src/test/java/com/recruit/common/FirstTest.java
+++ recruit-web-biz/src/test/java/com/recruit/common/FirstTest.java
@@ -4,16 +4,13 @@ import com.recruit.base.BaseTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.logging.Logger;
-
 /**
  * Created by zhuangjt on 2017/3/13.
  */
 @Slf4j
 public class FirstTest extends BaseTest {
-
     @Test
     public void m0() {
-        log.info("=== Testing ===");
+        log.error("=== Testing ===");
     }
 }
